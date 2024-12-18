107d11eb433690aad759a247d8a7f99ebb9bf83e
Will Darkins <will.darkins@adcellerant.com>
2024-12-18 15:01:40 -0800

    wrote some functionality for login... button clicking and so fourth

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,10 @@ test('Simple basic test', async ({ page }) => {
     const pageTitle = await page.locator('#nava')
     await expect(pageTitle).toContainText(' PRODUCT STORE')
 })
+
+test('Clicking on Elements', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/index.html#')
+    await page.click('#login2')
+    const logInButton = page.getByRole('button', { name: 'Log in' })
+    await logInButton.click()
+})
