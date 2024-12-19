92dc4b671c3be9095e1bb93d014e5a85ee0bbf59
Will Darkins <will.darkins@adcellerant.com>
2024-12-18 17:03:35 -0800

    added validation of dialog box when having empty fields for login form

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,5 +10,12 @@ test('Clicking on Elements', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/index.html#')
     await page.click('#login2')
     const logInButton = page.getByRole('button', { name: 'Log in' })
+    
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert')
+        expect(dialog.message()).toContain('Please fill out Username and Password.')
+        await dialog.accept(); 
+    })
+
     await logInButton.click()
 })
