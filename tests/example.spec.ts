56fb2c6ca0783384cc4951f665a5bfd2f79fd0b3
Will Darkins <will.darkins@adcellerant.com>
2024-12-18 17:17:58 -0800

    created negative login scenario with error message validation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,12 +10,24 @@ test('Clicking on Elements', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/index.html#')
     await page.click('#login2')
     const logInButton = page.getByRole('button', { name: 'Log in' })
-    
     page.on('dialog', async dialog => {
         expect(dialog.type()).toContain('alert')
         expect(dialog.message()).toContain('Please fill out Username and Password.')
         await dialog.accept(); 
     })
+    await logInButton.click()
+})
 
+test.only('Working with inputs... Negative login scenario', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/index.html')
+    await page.click('#login2')
+    await page.fill('#loginusername', 'some username')
+    await page.fill('#loginpassword', 'some password')
+    const logInButton = page.getByRole('button', { name: 'Log in' })
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert')
+        expect(dialog.message()).toContain('User does not exist.')
+        await dialog.accept(); 
+    })
     await logInButton.click()
 })
