1f3f459d8eaf43a69cbbb556a4042f141d9e936e
Will Darkins <will.darkins@adcellerant.com>
2024-12-30 13:18:37 -0800

    created LoginPage.ts to form POM

---
+++ page-objects/LoginPage.ts
@@ -0,0 +1,20 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+    //Define Selectors
+    readonly page: Page
+    readonly usernameInput: Locator
+    readonly passwordInput: Locator
+    readonly loginSubmitButton: Locator
+    readonly closeButton: Locator
+    
+    //Init selectors using constructor
+    constructor(page: Page) {
+        this.page = page
+        this.usernameInput = page.locator('#loginusername')
+        this.passwordInput = page.locator('#loginpassword')
+        this.loginSubmitButton = page.locator('text=Log in')
+        this.closeButton = page.locator('text=Log in')
+    }
+    //Define page methods
+}
